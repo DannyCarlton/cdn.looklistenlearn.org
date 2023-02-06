52c44a1d6821afc7817119c8648a3913ba3ecba0
Danny Carlton <webdesign@dannycarlton.com>
2023-02-06 08:22:22 -0600

    Initial upload

---
+++ languages/map.php
@@ -0,0 +1,16 @@
+<?php
+
+$Lang=[];
+
+$Lang['french1']=[];
+$Lang['french1']['title']='French 1';
+$Lang['french1']['Chapters']=[];
+$Lang['french1']['Chapters'][1]=[];
+$Lang['french1']['Chapters'][1]['title']='Chapter One';
+$Lang['french1']['Chapters'][1]['vocab']='languages/French1/VOCAB1.DAT';
+$Lang['french1']['Chapters'][2]=[];
+$Lang['french1']['Chapters'][1]['title']='Chapter Two';
+$Lang['french1']['Chapters'][2]['vocab']='languages/French1/VOCAB2.DAT';
+
+
+?>
